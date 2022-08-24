0dc4165f86deef78301c55adfdda9ad0d694330c
Tydomx <michaelthai16@gmail.com>
2022-08-23 19:15:37 -0500

    Added proficiencies of front/backend of resume

--- src/assets/components/pages/Resume.js
+++ src/assets/components/pages/Resume.js
@@ -4,9 +4,30 @@ export default function Resume() {
 	return (
 		<div>
 			<h2>Resume</h2>
-			<p>
-				Resume here
-			</p>
+
+			<p>Download the <a href="">resume</a></p>
+			<div>
+				<h3>Front-end Proficiencies</h3>
+				<ul>
+					<li>JavaScript</li>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JQuery</li>
+					<li>React</li>
+					<li>Bootstrap</li>
+					<li>responsive design</li>
+				</ul>
+			</div>
+			<div>
+				<h3>Back-end Proficiencies</h3>
+				<ul>
+					<li>APIs</li>
+					<li>Node</li>
+					<li>Express</li>
+					<li>MySQL, Sequelize</li>
+					<li>MongoDB</li>
+				</ul>
+			</div>
 		</div>
 
 	)
