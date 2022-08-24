7c2a7135897522a17121909619665abf7419a376
Tydomx <michaelthai16@gmail.com>
2022-08-23 20:22:10 -0500

    Added portfolio projects and links

--- src/assets/components/pages/Portfolio.js
+++ src/assets/components/pages/Portfolio.js
@@ -1,63 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import './portfolio.css';
 
 export default function Portfolio() {
 	const projectPortfolioArr = [
 		{
-			name: '',
-			image: '',
-			repo: '',
-			deployed: ''
+			name: 'Run Buddy',
+			image: 'run_buddy_screenshot.png',
+			repo: 'https://github.com/Tydomx/run-buddy',
+			deployed: 'https://tydomx.github.io/run-buddy/'
 		},
 		{
-			name: '',
-			image: '',
-			repo: '',
-			deployed: ''
+			name: 'barkr meetup',
+			image: 'barkr.png',
+			repo: 'https://github.com/Tydomx/barkr-meetup',
+			deployed: 'https://barkr-meetup.herokuapp.com/'
 		},
 		{
-			name: '',
-			image: '',
-			repo: '',
-			deployed: ''
+			name: 'Marvel Project',
+			image: 'MarvelProjectScreenshot.png',
+			repo: 'https://github.com/Tydomx/Marvel_project',
+			deployed: 'https://tydomx.github.io/Marvel_project/'
 		},
 		{
-			name: '',
-			image: '',
-			repo: '',
-			deployed: ''
+			name: 'Code Quiz',
+			image: 'codeQuiz.png',
+			repo: 'https://github.com/Tydomx/Code_Quiz',
+			deployed: 'https://tydomx.github.io/Code_Quiz/'
 		},
 		{
-			name: '',
-			image: '',
-			repo: '',
-			deployed: ''
+			name: 'Weather Dashboard',
+			image: 'weatherDashboard.png',
+			repo: 'https://github.com/Tydomx/weather-dashboard',
+			deployed: 'https://tydomx.github.io/weather-dashboard/'
 		},
 		{
-			name: '',
-			image: '',
-			repo: '',
-			deployed: ''
+			name: 'Team Profile Generator',
+			image: 'teamProfileGenerator.png',
+			repo: 'https://github.com/Tydomx/team-profile-generator',
+			deployed: 'https://www.youtube.com/watch?v=8-oJ8RnlSO4&feature=youtu.be'
 		},
-	]
-
+	];
 
 	return (
-		<div>
-			<h2>Portfolio Projects: </h2>
-			<div>
+		<section>
+			<h2 className="portfolioTitle">Portfolio Projects: </h2>
+			<div className="containerProjects">
 				{projectPortfolioArr.map(({ name, image, repo, deployed, index }) => (
-					<div className="col-lg-6 col-sm-10" key={index}>
+					<div className="col-lg-4 col-sm-6" key={index}>
+						<h2 className="portfolioName">{name}</h2>
 						<a href={deployed} target='_blank' rel="noreferrer">
-							<h2>{name}</h2>
-							<img className="portfolioProjectImage" src={require(`../../../assets/images/${image}`)} alt={name} />
+							<img className="portfolioProjectImage flex-row"
+								src={require(`../../../assets/images/${image}`)}
+								alt={name}
+								style={{ width: "65%", height: "325px" }} />
 						</a>
-						<a href={repo} target='_blank' rel="noreferrer">
-							<h3>Repository</h3>
-							<p>{repo}</p>
-						</a>
+						<div className="repoLink">
+							<h3>Repo Link</h3>
+							<a href={repo} target='_blank' rel="noreferrer">
+								<p>{repo}</p>
+							</a>
+						</div>
+
 					</div>
 				))}
 			</div>
-		</div>
+		</section>
 	)
 }
