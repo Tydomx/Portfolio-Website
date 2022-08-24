173e8697cec78df565a5cdc355b7ed23bf93c6a4
Tydomx <michaelthai16@gmail.com>
2022-08-23 20:37:38 -0500

    Fixing bug for CSS file not connecting

--- src/assets/components/pages/Portfolio.js
+++ src/assets/components/pages/Portfolio.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import './portfolio.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 export default function Portfolio() {
 	const projectPortfolioArr = [
